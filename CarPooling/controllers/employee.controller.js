b33bf7da0aacb2c33f8b4209be0c98476bb3266f
= <=>
2024-05-20 19:46:43 +0530

    add dependencies

--- CarPooling/controllers/employee.controller.js
+++ CarPooling/controllers/employee.controller.js
@@ -1,23 +1,17 @@
 const employeeModel = require("../models/employee.model/employee.model");
-async function getEmployees(req, res)
-{
-    try
-    {
-        const {username, password, email, mobilenumber, vehiclemodel, vehiclenumber, verified, active} = req.body;
-        const employee = await employeeModel.create({
-            username:username,
-            password:password,
-            email:email,
-            mobilenumber:mobilenumber,
-            vehiclemodel:vehiclemodel,
-            vehiclenumber:vehiclenumber,
-            verified:verified
-        });
-        console.log("Inserted to employee collection");
-        res.status(200).json(employee);
+
+class EmployeeController {
+  async getEmployees(req, res) {
+    try {
+      const employee = await employeeModel.create(req.body);
+      console.log("Inserted to employee collection");
+      res.status(200).json(employee);
+    } catch (err) {
+      res.status(404).json({
+        message: "Error in inserting document to employee collection",
+      });
     }
-    catch(err)
-    {
-        res.status(404).json({message:"Error in inserting document to employee collection"});
-    }
+  }
 }
+
+module.exports = new EmployeeController();
