c1076a72a2ef87b388e053a71278e1ef3e45a2d4
= <=>
2024-05-22 22:21:20 +0530

    login controllers

--- CarPooling/backend/controllers/login.controller.js
+++ CarPooling/backend/controllers/login.controller.js
@@ -1,18 +1,59 @@
-const customerModel = require("../models/customer.model");
+const userModel = require("../models/users.model");
+const bcryptjs = require("bcryptjs");
 
 class LoginController {
-  async saveCustomer(req, res) {
+  async userLogin(req, res) {
     try {
-      const customer = {
-        customerid: req.body.customerid,
-        customername: req.body.customername,
-        emailid: req.body.emailid,
-        mobilenumber: req.body.mobilenumber,
-      };
-      const cust = customerModel.create(customer);
-      res.status(200).json({ msg: "Inserted sucessfully" });
+      const { userName, password } = req.body;
+      const user = await userModel.findOne({ userName });
+      if (user) {
+        const compare = await bcryptjs.compare(password, user.password);
+        if (compare) {
+          return res.status(200).json({ msg: "Logged in successfully" });
+        }
+        return res.status(200).json({ msg: "Incorrect password" });
+      }
+      return res.status(200).json({ msg: "Incorrect username or password" });
     } catch (err) {
-      res.status(500).json({ msg: "failed to create customer " + err.message });
+      res.status(500).json({ msg: err.message });
+    }
+  }
+  async userRegister(req, res) {
+    req.employeeDetails = {};
+    if (req.body.isEmployee) {
+      req.employeeDetails = {
+        vehicleModel: req.body.vehicleModel,
+        vehicleNumber: req.body.vehicleNumber,
+      };
+    }
+    try {
+      //Query to check if the registering user already exists
+      const user = await userModel.findOne({
+        $or: [
+          { userName: req.body.userName },
+          { emailId: req.body.emailId },
+          { phoneNumber: req.body.phoneNumber },
+        ],
+      });
+
+      if (user) return res.status(200).json({ msg: "User already exists" });
+
+      //If user does not exists Hash the password and register the user into database
+      //Hashing
+      const salt = await bcryptjs.genSalt(10);
+      const hashedPassword = await bcryptjs.hash(req.body.password, salt);
+
+      const newUser = await userModel.create({
+        userName: req.body.userName,
+        emailId: req.body.emailId,
+        phoneNumber: req.body.phoneNumber,
+        password: hashedPassword,
+        isEmployee: req.body.isEmployee,
+        employeeDetails: req.employeeDetails,
+      });
+      return res.json(newUser);
+    } catch (err) {
+      res.status(500).json({ msg: err.message });
     }
   }
 }
---
+++ CarPooling/backend/routes/getstarted.js
@@ -0,0 +1,8 @@
+const loginController = require("../controllers/login.controller");
+const express = require("express");
+const route = express.Router();
+
+route.post("/login", loginController.userLogin);
+route.post("/signup", loginController.userRegister);
+
+module.exports = route;
