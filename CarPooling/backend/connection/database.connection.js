1c290310f836661f25c60b1a8c73e7bba3212f79
madhukar666 <anveshnallamadha@gmail.com>
2024-09-12 23:25:27 +0530

    database connection

--- CarPooling/backend/connection/database.connection.js
+++ CarPooling/backend/connection/database.connection.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
-const MONGO_URI = process.env.MONGO_URI | undefined;
+const MONGO_URI = process.env.MONGO_URI;
 
 const mongoose = require("mongoose");
-const connection = mongoose.connect(
-  MONGO_URI);
+const connection = mongoose.connect(MONGO_URI);
 connection
     .then(() => {
         console.log("connection with carpool database is successful");
